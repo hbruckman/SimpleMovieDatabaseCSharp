e949318aa503735483749ef19f188c7bf7b6c8be
hbruckman <henry.bruckman@gmail.com>
2025-11-08 14:21:45 -0400

    Testing Abcs+Smdb Script Attempt #3

--- git-smdb.bat
+++ git-smdb.bat
@@ -37,12 +37,12 @@ goto SYNC
 
 :FIRST_RUN
 echo First run: adding %REMOTE_NAME%/%SUBTREE_REF% under %PREFIX%
-git subtree add --prefix "%PREFIX%" %REMOTE_NAME% "%SUBTREE_REF%" --squash -m "%SUBTREE_MSG%" || (echo ERROR: subtree add failed & popd & exit /b 1)
+git subtree add --prefix="%PREFIX%" %REMOTE_NAME% "%SUBTREE_REF%" --squash -m "%SUBTREE_MSG%" || (echo ERROR: subtree add failed & popd & exit /b 1)
 goto POST_SYNC
 
 :SYNC
 echo Sync run: pulling %REMOTE_NAME%/%SUBTREE_REF% into %PREFIX%
-git subtree pull --prefix "%PREFIX%" %REMOTE_NAME% "%SUBTREE_REF%" --squash -m "%SUBTREE_MSG%" || (echo ERROR: subtree pull failed & popd & exit /b 1)
+git subtree pull --prefix="%PREFIX%" %REMOTE_NAME% "%SUBTREE_REF%" --squash -m "%SUBTREE_MSG%" || (echo ERROR: subtree pull failed & popd & exit /b 1)
 
 :POST_SYNC
 git push origin %SMDB_BRANCH% || (echo ERROR: push failed & popd & exit /b 1)
